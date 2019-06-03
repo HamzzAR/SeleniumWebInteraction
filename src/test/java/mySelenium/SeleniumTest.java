71cdbe947cc781f08a15dc3908b000bf7a5ee68b
HamzzAR <hamza_razeq@hotmail.co.uk>
2019-06-03 15:22:14 +0100

    First commit

---
+++ src/test/java/mySelenium/SeleniumTest.java
@@ -0,0 +1,40 @@
+package mySelenium;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+	public static WebDriver driver;
+	String url = "http://www.Google.com";
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@AfterClass
+	public static void teardown() throws InterruptedException {
+		Thread.sleep(1000);
+		driver.quit();
+	}
+	
+	@Test
+	public void methodTest() {
+		driver.get(url);
+		WebElement we = driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input"));
+		we.sendKeys("Cute Kittens");
+		we.sendKeys(Keys.ENTER);
+	}
+
+}
