e1848b652c7dc9e1dda78ceb010e4ce9ac14e5e0
HamzzAR <hamza_razeq@hotmail.co.uk>
2019-06-03 17:25:04 +0100

    using mouse events, writing letters

--- src/test/java/mySelenium/SeleniumTest.java
+++ src/test/java/mySelenium/SeleniumTest.java
@@ -5,12 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 public class SeleniumTest {
 	public static WebDriver driver;
@@ -25,10 +28,11 @@ public class SeleniumTest {
 	
 	@AfterClass
 	public static void teardown() throws InterruptedException {
-		Thread.sleep(1000);
-		driver.quit();
+//		Thread.sleep(5000);
+//		driver.quit();
 	}
 	
+	@Ignore
 	@Test
 	public void methodTest() {
 		driver.get(url);
@@ -36,6 +40,27 @@ public class SeleniumTest {
 		we.sendKeys("Cute Kittens");
 		we.sendKeys(Keys.ENTER);
 		
+		we = driver.findElement(By.xpath("//*[@id=\"imagebox_bigimages\"]/g-section-with-header/div[1]/h3/a"));
+		assertEquals("Images for Cute Kittens",we.getText());
+	}
+	
+	@Test
+	public void methodTest2() {
+		driver.get("https://www.youidraw.com/apps/painter/");
+		Actions action =  new Actions(driver);
+		//x,y
+		action.moveToElement(driver.findElement(By.id("brush"))).click().perform();
+		action.moveToElement(driver.findElement(By.id("catch")),50,20).clickAndHold().moveByOffset(1,300).release().perform();
+		action.moveToElement(driver.findElement(By.id("catch")),200,20).clickAndHold().moveByOffset(1,300).release().perform();
+		action.moveToElement(driver.findElement(By.id("catch")),50,150).clickAndHold().moveByOffset(150,0).release().perform();
+		
+		action.moveToElement(driver.findElement(By.id("catch")),300,20).clickAndHold().moveByOffset(-50,300).release().perform();
+		action.moveToElement(driver.findElement(By.id("catch")),300,20).clickAndHold().moveByOffset(50,300).release().perform();
+		action.moveToElement(driver.findElement(By.id("catch")),280,220).clickAndHold().moveByOffset(65,0).release().perform();
+		
+		action.moveToElement(driver.findElement(By.id("catch")),360,310).clickAndHold().moveByOffset(65,-280).release().perform();
+		action.moveToElement(driver.findElement(By.id("catch")),280,).clickAndHold().moveByOffset(65,-280).release().perform();
 	}
 
 }
+
